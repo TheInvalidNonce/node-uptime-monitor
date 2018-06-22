ae69dcb3951e4aebfab48c073ad09781a9faaaea
Janusz Szubert <theinvalidnonce@gmail.com>
2018-06-22 16:59:55 -0400

    Add services foreach function

--- index.js
+++ index.js
@@ -22,9 +22,57 @@ const pingService = (url, callback) => {
       callback(resp.timingPhases.firstByte)
     }
     else {
-      callback('OUTAGE!!!')
+      callback('OUTAGE')
     }
   }
   )
 }
+
+const pingInterval = 5*1000*60 // 5 minute interval
+let serviceStatus = {}
+
+services.forEach( service => {
+
+  serviceStatus[service.url] = {
+    status: 'OPERATIONAL', // initialize all services as operational when we start
+    responseTimes: [], // array containing the responses times for last 3 pings
+    timeout: service.timeout, // load up the timeout from the config 
+  }
+
+  setInterval( () => {
+    pingService(service.url, serviceResponse => {
+      if (serviceResponse === 'OUTAGE' && serviceStatus[service.url].status !== 'OUTAGE') {
+        // only update and post to Slack on state change
+        serviceStatus[service.url].status = 'OUTAGE'
+        postToSlack(service.url)
+      }
+      else {
+        let responseTimes = serviceStatus[service.url].responseTimes
+        responseTimes.push(serviceResponse)
+      }
+
+      // check degraded performance if we have 3 responses so we can average them
+
+      if (responseTimes.length > 3) {
+      // remove the oldest response time from the beginning of array
+        responseTimes.shift()
+
+      // compute average of last 3 response times
+      let avgRespTime = responseTimes.reduce( (a, b) => a + b, 0 ) / responseTimes.length
+      let currService = serviceStatus[service.url]
+      
+      if (avgResTime > currService.timeout && currService.status !== 'DEGRADED') {
+        currService.status = 'DEGRADED'
+        postToSlack(service.url)
+      } 
+      else if (avgResTime < currService.timeout && currService.status !== 'OPERATIONAL') {
+        currService.status = 'OPERATIONAL'
+        postToSlack(service.url)
+        }
+      }
+
+    })
+
+  }, pingInterval)
+})
 
