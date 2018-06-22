54cd25398243d3dda72a41dd228bfeab5ca5ff1e
Janusz Szubert <theinvalidnonce@gmail.com>
2018-06-22 16:22:49 -0400

    Add module.exports

---
+++ services.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {
+    // url of the service to check
+    url: 'https://zumic.com',
+    // threshold in milliseconds above which is considered poor performance
+    timeout: 500 
+  }
+]
